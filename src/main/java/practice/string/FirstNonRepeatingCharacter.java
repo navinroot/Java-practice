b184f6e82773492e6a6877834c9623f45b24c598
navinroot <navinaryan6@gmail.com>
2019-10-12 13:11:48 +0530

    adding practice files

--- src/main/java/practice/string/FirstNonRepeatingCharacter.java
+++ src/main/java/practice/string/FirstNonRepeatingCharacter.java
@@ -2,6 +2,28 @@ package practice.string;
 
 public class FirstNonRepeatingCharacter {
 
+    private static void nonRepeatingCharacter( String str){
+        int index =256;
+        char[] chars = new char[index];
+
+        for (char c : str.toCharArray()){
+            chars[c] +=1;
+        }
+        for(char c : str.toCharArray()){
+            if(chars[c] ==1){
+                System.out.println(c);
+                break;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+            nonRepeatingCharacter("geeksforgeek");
+
+    }
+
 
 
 }
